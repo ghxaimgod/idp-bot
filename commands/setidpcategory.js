e705bc072e6233eef8ccdc413f77a9e303ace1c2
ghxaimgod <ghxaimgod@gmail.com>
2026-06-29 12:02:00 +0530

    Update setidpcategory.js

--- commands/setidpcategory.js
+++ commands/setidpcategory.js
@@ -1,67 +1,20 @@
-const fs = require("fs");
-
-const path = "./data/config.json";
-
-const {
-SlashCommandBuilder,
-ChannelType
-} = require("discord.js");
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
+const dataManager = require('../utils/dataManager');
 
 module.exports = {
-
-data: new SlashCommandBuilder()
-
-.setName("setidpcategory")
-
-.setDescription("Set IDP Category")
-
-.addChannelOption(option =>
-
-option
-
-.setName("category")
-
-.setDescription("Select Category")
-
-.addChannelTypes(ChannelType.GuildCategory)
-
-.setRequired(true)
-
-),
-
-async execute(interaction) {
-
-const category =
-interaction.options.getChannel("category");
-
-let data = {};
-
-if (fs.existsSync(path)) {
-
-data = JSON.parse(
-fs.readFileSync(path)
-);
-
-}
-
-data[interaction.guild.id] = {
-
-categoryId: category.id
-
-};
-
-fs.writeFileSync(
-path,
-JSON.stringify(data, null, 2)
-);
-
-await interaction.reply({
-
-content:
-`✅ IDP Category Set To **${category.name}**`
-
-});
-
-}
-
+    data: new SlashCommandBuilder()
+        .setName('setidpcategory')
+        .setDescription('Set the Tournament Category where Group Channels exist.')
+        .addChannelOption(option => 
+            option.setName('category')
+                .setDescription('Select the target category channel')
+                .addChannelTypes(ChannelType.GuildCategory)
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    async execute(interaction) {
+        const category = interaction.options.getChannel('category');
+        dataManager.saveConfig(interaction.guildId, { categoryId: category.id });
+        
+        await interaction.reply({ content: `✅ Successfully set targeting updates category target to **${category.name}**!`, ephemeral: true });
+    }
 };
